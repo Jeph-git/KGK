4fb85bfb36026ea5140bfc5bea477a063745476f
Johan <langaas.johan@gmail.com>
2024-10-15 13:02:11 +0200

    Removed developer tools

--- app.py
+++ app.py
@@ -42,6 +42,6 @@ def home():
     # Pass the messages to the template
     return render_template('index.html', form=form, messages_dict=messages_dict)
 
-if __name__ == '__main__':
-    # app.run(debug=True, host= '192.168.10.222') # This one for running on local network
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     # app.run(debug=True, host= '192.168.10.222') # This one for running on local network
+#     app.run(debug=True)
